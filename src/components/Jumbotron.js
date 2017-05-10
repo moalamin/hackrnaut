62a3c3907828e74ad0823cb6c6704eae000fcdcb
Mo Amin <m.alamin92@gmail.com>
2017-05-10 00:21:10 -0400

    cover

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -9,7 +9,8 @@ class Jumbotron extends Component {
           <style>{`.cls-1 {fill: url(#radial-gradient)} `}</style>
           <radialGradient id="radial-gradient" cx="640" cy="512" r="190.28" gradientUnits="userSpaceOnUse"><stop id="color-one" offset="0" stopColor="#29abe2"/><stop id="color-two" offset="1" stopColor="#0071bc"/></radialGradient></defs><title>hackrlogo</title><path className="cls-1" d="M509.57,285.59h31.57a15,15,0,0,1,15,15V456.21a15,15,0,0,0,15,15H708.86a15,15,0,0,0,15-15V300.59a15,15,0,0,1,15-15h31.57a15,15,0,0,1,15,15V723.41a15,15,0,0,1-15,15H738.86a15,15,0,0,1-15-15V537.77a15,15,0,0,0-15-15H571.14a15,15,0,0,0-15,15V723.41a15,15,0,0,1-15,15H509.57a15,15,0,0,1-15-15V300.59A15,15,0,0,1,509.57,285.59Z" transform="translate(-494.57 -285.59)"/>
         </svg>*/}
-        <h1>Welcome to my site!</h1>
+        <h1 className="jumbo-logo--text" >Hello, I'm Mo.</h1>
+        <h2>I make websites.</h2>
       </div>
     );
   }

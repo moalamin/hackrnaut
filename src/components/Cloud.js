f39fd52cacc38e223186dd5af29856e154b490d3
Mo Amin <m.alamin92@gmail.com>
2017-05-24 23:36:11 -0400

    import into index scss instead of importing into react component

--- src/components/Cloud.js
+++ src/components/Cloud.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../styles/Cloud.scss';
 
 module.exports = (props) => {
   console.log(props)
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,19 +1,12 @@
 import React, {Component} from 'react';
 import '../styles/Jumbotron.scss';
-import Cloud from './Cloud'
 
 class Jumbotron extends Component {
   render() {
     return (
       <div className="jumbo-logo">
         <h1 className="jumbo-logo__text" >Hello, I'm Mo.</h1>
-        <h2 className="jumbo-logo__text-small">I am a software developer from New York City.
-          <br/>
-           I earned my BA in Computer Science at Hunter College.
-           <br/>
-           I have experience building and maintaing web applications that are used daily by millions of users.
-           </h2>
-        <Cloud top={"20"}/>
+        <h2 className="jumbo-logo__text-small">I am a software developer from New York City. </h2>
       </div>
     );
   }

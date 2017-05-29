4b0998bc59b919678ba47bd15e899c7927e7f91b
Mo Amin <m.alamin92@gmail.com>
2017-05-29 18:01:32 -0400

    more on about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 export default () => (
-  <div className='about'>
-    <div className='about-section about-section-clip'>
-      <h1>/About</h1>
+  <div className='pure-g about bg-dark'>
+    <div className='pure-u-md-1-3 pure-u-1 about--portrait-container'>
+      <img src='/me.png' alt='Portrait' className='pure-img'/>
     </div>
-    <div className='about-section about-section-behind'>
-      <h1 className='very-large'>/About</h1>
-    </div>
-  < /div>
+    <div className="pure-u-1 pure-u-md-2-3 about--blurb font-light">
+      <div className='pure-u-1 pure-u-md-3-4 about--blurb-container'>
+        <h1>I do Full-Stack.</h1>
+        <p>
+          I graduated in 2015 with a Bachelor's Degree in Computer Science.
+          Since graduating I have been building and maintaining websites full time.
+          I worked at <a href='https://www.wirecare.com' target='_blank'>Wirecare</a> where I helped them
+          rewrite thier website in a RAILS stack over the course of a year. I am currently working at <a href='https://www.law360.com'>Law360</a>
+          where I am also doing full-stack work. My favorite languages are Javascript and Ruby.
+          <br/>
+        </p>
+        <h2>Technical Skills</h2>
+        <p><strong>Languages:&nbsp;</strong>Ruby, Javascript, HTML, CSS</p>
+      </div>
+  </div>
+</div>
 );
--- src/components/Cloud.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-module.exports = (props) => {
-  console.log(props)
-  return (<svg className="cloud" id="Layer_1" data-name="Layer 1" style={{top:props.top, left:props.top}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 408 199"><defs><style>{` .cls-1{fill:#f2f2f2;} `}</style></defs><title>cloud</title><path className="cls-1 cloud__path" d="M633,458a48.11,48.11,0,0,0-5.13.28,69.69,69.69,0,0,0-45.47-40.66,96.59,96.59,0,0,0-128-47.93h0C440.37,401.8,464,424,464,424c-26.82-8.32-25.41-40.61-25-45.4a96.9,96.9,0,0,0-26.29,27.76,74.49,74.49,0,0,0-87,57.68c-.54,0-1.08,0-1.62,0a45,45,0,0,0-45,45c0,22.47,16.47,41.64,38,45H633a48,48,0,0,0,0-96Z" transform="translate(-276 -358)"/></svg>)
-}
